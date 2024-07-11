49d36baf99baf0addc0610e0ab30b1f449378363
Semah <sameh.naski@esprit.tn>
2024-07-11 18:33:48 +0100

    Role entity without mapping

---
+++ book.network/src/main/java/com/semah/book/network/role/Role.java
@@ -0,0 +1,33 @@
+package com.semah.book.network.role;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
