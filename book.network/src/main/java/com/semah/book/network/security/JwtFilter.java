3147907294bb311683f3fad5434ea557209c3e08
Semah <sameh.naski@esprit.tn>
2024-07-23 02:37:11 +0100

    authenticationProvider

---
+++ book.network/src/main/java/com/semah/book/network/security/JwtFilter.java
@@ -0,0 +1,9 @@
+package com.semah.book.network.security;
+
+import jakarta.servlet.Filter;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class JwtFilter implements Filter {
+}
--- book.network/src/main/java/com/semah/book/network/security/SecurityConfig.java
+++ book.network/src/main/java/com/semah/book/network/security/SecurityConfig.java
@@ -6,9 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +22,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 public class SecurityConfig {
-
     private final JwtFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
